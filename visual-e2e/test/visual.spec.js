383ce6f4b6f1706b8860bc8add6c5f739406e65a
nadvolod <nadvolod@gmail.com>
2021-01-12 17:31:00 -0500

    1st working visual tests

---
+++ visual-e2e/test/visual.spec.js
@@ -0,0 +1,7 @@
+describe('Home page', () => {
+    it('should look correct visually', () => {
+        browser.url('https://saucedemo.com');
+        browser.execute('/*@visual.init*/', 'My Visual Test');
+        browser.execute('/*@visual.snapshot*/', 'Login Page');
+    });
+});
