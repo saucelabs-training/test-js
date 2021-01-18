b278a21d872e29541932007fe835a52e579663c4
christian-bromann <mail@christian-bromann.com>
2021-01-18 18:46:34 +0100

    fix config file

--- solutions/02_Setup/webdriverio/wdio.conf.js
+++ solutions/02_Setup/webdriverio/wdio.conf.js
@@ -67,7 +67,7 @@ exports.config = {
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'trace',
-    outputDir: path.resolve(process.cwd(), 'logs')
+    outputDir: path.resolve(process.cwd(), 'logs'),
     //
     // Set specific log levels per logger
     // loggers:
