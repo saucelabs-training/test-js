6bc72298cdf76f55a1f2be7c4708cd4bf969d76b
christian-bromann <mail@christian-bromann.com>
2021-01-18 19:56:37 +0100

    add solution for puppeteer

---
+++ solutions/02_Setup/puppeteer/tests/example.test.js
@@ -0,0 +1,25 @@
+describe('Login scenarios', () => {
+    let page;
+
+    beforeAll( async() => {
+        page = (await browser.pages())[0];
+    });
+
+    test('Good credentials pass', async () => {
+		await page.goto('https://react-redux.realworld.io/#/login');
+
+		const username = await page.$('[type="email"]')
+		await username.type('testjssummit@gmail.com')
+
+		const password = await page.$('[type="password"]')
+		await password.type('password')
+
+		const btn = await page.$('button[type="submit"]')
+		await btn.click()
+
+		await page.waitForSelector('[href="#@testjssummit"]')
+		const usernameLink = await page.$('[href="#@testjssummit"]')
+		expect(await (await usernameLink.getProperty('textContent')).jsonValue())
+			.toBe('testjssummit')
+	})
+})
