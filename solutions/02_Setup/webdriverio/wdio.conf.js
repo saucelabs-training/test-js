2b5b086ccdd6d5e4b0a0e3d538f362ef19b993a8
Christian Bromann <mail@christian-bromann.com>
2021-01-27 17:04:45 +0100

    use base url

--- solutions/02_Setup/webdriverio/wdio.conf.js
+++ solutions/02_Setup/webdriverio/wdio.conf.js
@@ -93,7 +93,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'https://react-redux.realworld.io',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 20000,
